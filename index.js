905683c7db75fb2efeca3b435e805d973295d88d
KrutikGadhiya <66325627+KrutikGadhiya@users.noreply.github.com>
2021-06-28 11:44:55 +0530

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const db = require('./db');
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(bodyParser.json());
 
 // routes
